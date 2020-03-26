f0f9ffc2364dc4e3dbcfd33bc1a6e69bd10ec5e5
minorutaso <minorutaso@gmail.com>
2020-03-26 21:22:06 +0900

    コメントのビューを修正

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,11 +1,20 @@
 $(function() {
     function readURL(input) {
+      console.log("sss");
       if (input.files && input.files[0]) {
         var reader = new FileReader();
+        console.log("hhh");
+
         reader.onload = function (e) {
+          console.log("yy");
+
           $('#img_prev').attr('src', e.target.result);
+          console.log(src);
+
         }
         reader.readAsDataURL(input.files[0]);
+      }else{
+        console.log("aaa");
       }
     }
     $("#effect_image").change(function(){
