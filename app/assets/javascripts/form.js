bacc47e90cb2085faa4755c534bfc5c591048d20
minorutaso <minorutaso@gmail.com>
2020-03-26 19:58:48 +0900

    JSが発火

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -5,12 +5,11 @@ $(function() {
         reader.onload = function (e) {
           $('#img_prev').attr('src', e.target.result);
         }
-        console.log("あかん！")
         reader.readAsDataURL(input.files[0]);
       }
     }
     $("#effect_image").change(function(){
-        console.log("あかん！")
+
       readURL(this);
     });
    });
