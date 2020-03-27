7c5459d6687b0dae9d6b33c3b1de8a44a4114552
minorutaso <minorutaso@gmail.com>
2020-03-27 11:21:10 +0900

    プレビュー

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,15 +1,25 @@
 $(function() {
-  function readURL(input) {
-    if (input.files && input.files[0]) {
-      var reader = new FileReader();
-      reader.onload = function (e) {
-        $('#img_prev').attr('src', e.target.result);
-      }
-      reader.readAsDataURL(input.files[0]);
-    }
+  let buildHTML = (imageUrl) => {
+    let html = `<img id="img_prev" src="${imageUrl}">`
+    return html;
   }
-  $("#effect_image").change(function(){
 
-    readURL(this);
+  // function readURL(input) {
+  //   if (input.files && input.files[0]) {
+  //     var reader = new FileReader();
+  //     reader.onload = function (e) {
+  //       $('#img_prev').attr('src', e.target.result);
+  //     }
+  //     reader.readAsDataURL(input.files[0]);
+  //   }
+  // }
+
+  $("#effect_image").change(function(e){
+    let file = e.target.files[0];
+    const imageUrl = window.URL.createObjectURL(file)
+    $('.form-in').append(buildHTML(imageUrl));
+    
+
+    console.log(imageUrl)
   });
 });
