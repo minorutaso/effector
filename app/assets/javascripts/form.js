83fc8bf5cc84d0a4cbadb5c959a73c98c121c27a
minorutaso <minorutaso@gmail.com>
2020-03-27 11:42:45 +0900

    varにした

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,12 +1,12 @@
 $(function() {
-  let buildHTML = (imageUrl) => {
-    let html = `<img id="img_prev" src="${imageUrl}">`
+  var buildHTML = (imageUrl) => {
+    var html = `<img id="img_prev" src="${imageUrl}">`
     return html;
   }
 
   $("#effect_image").change(function(e){
-    let file = e.target.files[0];
-    const imageUrl = window.URL.createObjectURL(file)
+    var file = e.target.files[0];
+    var imageUrl = window.URL.createObjectURL(file)
     $('.form-in').append(buildHTML(imageUrl));
   });
 });
