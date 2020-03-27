a922b3bfdf6e45e751510c7fd52d55b34c5d50f0
minorutaso <minorutaso@gmail.com>
2020-03-27 13:00:52 +0900

    may

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -1,12 +1,32 @@
+$(document).on('turbolinks:load', ()=> {
 $(function() {
   let buildHTML = (imageUrl) => {
-    let html = `<img id="img_prev" src="${imageUrl}">`
+    let html = `<img width="250" height="250" id="img_prev" src="${imageUrl}">`
     return html;
   }
-
-  $("#effect_image").change(function(e){
+  function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        $('#img_prev').attr('src', e.target.result);
+      }
+      console.log("あかん！")
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+  $('#effect_image').change(function(e){
     let file = e.target.files[0];
-    let imageUrl = window.URL.createObjectURL(file)
+    const imageUrl = window.URL.createObjectURL(file);
+    $('#img_prev').remove();
     $('.form-in').append(buildHTML(imageUrl));
-  });
+    readURL(this);
+  })
+})
 });
+
+
+
+
+
+   
+  
