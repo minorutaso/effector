f173ea970822784707a78762313194ef8847f45d
minorutaso <minorutaso@gmail.com>
2020-04-01 21:42:07 +0900

    非同期　リロードしないと出来ない

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,3 +1,4 @@
+$(document).on('turbolinks:load', ()=> {
 $(function(){
     function buildHTML(comment){
         var html = `<div class="comment__view__box">
@@ -40,3 +41,4 @@ $(function(){
     })
     })
   })
+});
